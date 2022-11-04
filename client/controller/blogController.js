667be4f42be011b12000cf29f9850faafe4555a3
Michael Gartner <mclicks@gmail.com>
2022-11-03 17:15:14 -0700

    break apart blog and index controller

---
+++ client/controller/blogController.js
@@ -0,0 +1,83 @@
+const Blog = require('../model/blog')
+
+
+const blog_index = (req, res) => {
+    let isAuthenticated = req.oidc.isAuthenticated();
+    
+    if(isAuthenticated) {
+        Blog.find().sort({
+            createdAt: -1
+        }).then(result => {
+            res.render("posts", {
+                posts: result,
+                title: "Page",
+                isAuthenticated: isAuthenticated
+            })
+            // console.log(result)
+            // console.log(isAuthenticated)
+            })
+        } 
+}
+
+const show_posts = async(req, res) => {
+    let isAuthenticated = req.oidc.isAuthenticated();
+    res.render("posts", {
+        title: "Posts",
+        isAuthenticated: isAuthenticated,
+    })
+}
+
+const create_blog_post = async(req, res) => {
+    console.log("Data ", req.body);
+    const blog = new Blog(req.body);
+    blog.save()
+    .then(result => {
+        res.redirect('/posts');
+    })
+    .catch(e => {
+        console.log(e);
+    });
+}
+
+const edit_post = (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render('edit', {
+                post: result,
+                title: 'Edit Post',
+                isAuthenticated: req.oidc.isAuthenticated(),
+                user: req.oidc.user,});
+            })
+            .catch(e => {
+                console.log(e);
+                res.render('404', {title: 'Post Not Found'});
+            });
+    }
+
+const update_post = async (req, res) => {
+    const _id = req.params.id;
+    const doc = await Blog.findOne({_id});
+    console.log(req.body);
+    doc.overwrite({
+        title: req.body.title,
+        snippet: req.body.snippet,
+        body: req.body.body
+    })
+
+    await doc.save()
+        .then(() => {
+            res.redirect('/posts');
+        })
+        .catch(e => {
+            console.log("Error ", Error);
+        })
+}
+
+module.exports = {
+create_blog_post,
+show_posts,
+blog_index,
+edit_post,
+update_post,
+}
--- client/routes/router.js
+++ client/routes/router.js
@@ -2,25 +2,26 @@ var express = require("express");
 var router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 const axios = require('axios');
-const controller = require('../controller/controller');
+const blogController = require('../controller/blogController');
+const indexController = require('../controller/indexController');
 
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.get('/', controller.index);
-router.get('/contact', controller.contact);
-router.get('/loggedin', controller.logginedin);
-router.get('/login', requiresAuth(), controller.login);
-router.get('/posts', controller.blog_index);
-router.get('/submit/update/:id', controller.edit_post);
+router.get('/', indexController.index);
+router.get('/contact', indexController.contact);
+router.get('/loggedin', indexController.logginedin);
+router.get('/login', requiresAuth(), indexController.login);
+router.get('/posts', blogController.blog_index);
+router.get('/submit/update/:id', blogController.edit_post);
 // router.get('/posts', controller.show_posts);
 
-router.post('/submit', urlencodedParser, controller.create_blog_post);
-router.post('/submit/update/:id', urlencodedParser, controller.update_post);
+router.post('/submit', urlencodedParser, blogController.create_blog_post);
+router.post('/submit/update/:id', urlencodedParser, blogController.update_post);
 
 // trigger the endoint, and call the middleware, if the user is logged in or not
-router.get('/secured', requiresAuth(), controller.secured);
-router.get('/admin', requiresAuth(), controller.admin);
-router.get('/not_admin', controller.not_admin);
+router.get('/secured', requiresAuth(), indexController.secured);
+router.get('/admin', requiresAuth(), indexController.admin);
+router.get('/not_admin', indexController.not_admin);
 
 module.exports = router;
